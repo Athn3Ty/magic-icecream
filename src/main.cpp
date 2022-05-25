6db8eeeda2e0c0b4b0ac40ecbf8fdec47e71f2c5
Athn3Ty <Immanuel@3ty.dev>
2022-05-25 16:03:28 +0200

    creating a simple databese

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,22 @@
 // Tutorial : https://youtu.be/FcYs8wtzjVE?t=5880
 // Current time: 1:38.44
 #include "include/list.h"
+#include "include/database.h"
 
 int main(int arg_count, char *args[]) {
+    List simpleList;
+    Database data;
+    
     if(arg_count > 1) {
-        List simpleList;
         simpleList.name = string(args[1]);
         simpleList.print_menu();
+        data.write(simpleList.list);
+        data.read();
     }
     else{
         cout << "Username not supplied.. exeting the programm" << endl;
     }
 
+    
     return 0;
 }
